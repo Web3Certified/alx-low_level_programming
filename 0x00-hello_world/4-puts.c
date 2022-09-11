d1a309c73a9ae4e612024e9418494e9ebd61dafd
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-11 11:53:40 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * Description: main -printout a string to stdout
  * Return: 0 if success
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
