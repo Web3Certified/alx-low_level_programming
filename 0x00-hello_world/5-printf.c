4099de021cad3c041aa3006cce980804b996baf1
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-11 12:00:59 -0700

    5-printf.c commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - this one fuckn hard task
+ * Return: 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
