0844d5e99c44c4d6ebd465ddf5032e14217be7e1
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-14 10:11:16 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+char antony[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(antony[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
