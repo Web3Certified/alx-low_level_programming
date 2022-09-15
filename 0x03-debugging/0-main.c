ba8abe734485799a1d5ce68ece5f78e361581972
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-15 16:28:18 -0700

    0-main.c commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,7 @@
+#include "main.h"
+
 /**
- *main - tests function that prints if integer is positive or negative
+ *main - tests functions that prints if integer is positive or negative
  *Return: 0
  */
 int main(void)
