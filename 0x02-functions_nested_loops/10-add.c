5684bfd60c7869d62fedcb3a8fb42614d8334058
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-15 08:29:45 -0700

    add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - function that adds two integers and adds the result
+ *@n1: input integer 1
+ *@n2: input integer 2
+ *Return: result
+ */
+int add(int n1, int n2)
+{
+int result;
+result = n1 + n2;
+return (result);
+}
