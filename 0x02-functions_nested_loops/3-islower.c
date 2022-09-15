9d6e05e82cb7789b1271ec5666eb9e4cd7f8143b
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-14 18:21:26 -0700

    3-islower.c commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+
+/**
+ *_islower - check for lower case letter
+ *@c: character to check the case
+ *Return: 0 or 1
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
