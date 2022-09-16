ca46a2fa67d0ecc7198626ba263fd5888dd828ec
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-15 17:06:07 -0700

    main commmit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
 *main - causes an infinite loop
 *Return: 0
 */
+
 int main(void)
 {
 int i;
@@ -12,7 +13,7 @@ i = 0;
 /*
 *while (i < 10)
 *{
-*putchar(i):
+*putchar(i);
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
