21600b30b41a061ef1e320941d17bd21af06da9b
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-15 18:42:47 -0700

    largest_number commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,13 @@ int largest_number(int a, int b, int c)
 {
 int largest
 if (a >=c && a > c)
-}
-largest = a;
 {
+largest = a;
+}
 else if (b >= a && b > c)
 {
 largest = b;
-{
+}
 else
 {
 largest = c;
