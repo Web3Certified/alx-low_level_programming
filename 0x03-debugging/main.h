4a3dd57e310b995a7c2097833847d01bede044da
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-17 15:34:42 -0700

    largest_num commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,15 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-int add(int a, int b);
-void times_table(void);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+#endif /* MAIN.H  */
