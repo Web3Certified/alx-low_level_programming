f0db11f43c04624c3dd4bda4c60380e77da18627
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-17 18:14:38 -0700

    supper commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * _isupper - function that checksfor uppercase character.
  * @c: input value to check
  * Return: return 1 if c is uppercase 0 if otherwise
  */
-
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
