8320cccd6baccd9a98c61f82c3b6c06570c71423
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-17 20:25:14 -0700

    triangle commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_triangle - check for a digit
+ *@size: size of triangle
+ *Return: void
+ */
+void print_triangle(int size)
+{
+int i, j, k, columns;
+columns = size - 1;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < columns; j++)
+{
+_putchar(' ');
+}
+columns--;
+for (k = 0; k < (i + 1); k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else if (size <= 0)
+{
+_putchar('\n');	
+}
+}
