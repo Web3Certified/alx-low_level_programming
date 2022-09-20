c98bf98d2b01f7939e4a6940a4da497070b96f27
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-19 19:51:42 -0700

    resetion commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - write a function that takes a pointer to an int
+ *as parameter and updates the value it points to 9
+ *@n: pointer to an int
+ *Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
