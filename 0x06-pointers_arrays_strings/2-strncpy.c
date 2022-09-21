e70f3b9f956ac9aebfead94c424f5b32a76ce6e5
Web3Certified <108844675+Web3Certified@users.noreply.github.com>
2022-09-22 00:56:01 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_strncpy - function that copies a string
- *@dest: pointer to destination char
- *@src: pointer to source char
- *@n: number of bytes
- *Return: char
+ * _strncpy - function that copies a string
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: number of bytes
+ * Return: char
  */
 char *_strncpy(char *dest, char *src, int n);
 {
