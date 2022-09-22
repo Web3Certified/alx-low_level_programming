9272051f15b5f4d7fac6300f9002cbc933672b7e
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-21 17:21:37 -0700

    cmp commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/** _strcmp - function that compares two strings.
- * @s1: string 1
- * @s2: string 2
- * Return: 0 if matching, and ns1 - ns2 if not matching
+/**
+ *_strcmp - function that compares two strings.
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 0 if matching, and ns1 - ns2 if not matching
  */
 int _strcmp(char *s1, char *s2)
 {
