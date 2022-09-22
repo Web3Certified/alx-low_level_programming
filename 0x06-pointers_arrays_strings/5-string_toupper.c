65a40d94be8df248483920448c945e35f75d767b
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-21 18:26:32 -0700

    toupper commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,24 +7,11 @@
  */
 char *string_toupper(char *)
 {
-// the extra comments i wrote in this program
-// is to help me or whoever reads this code understand
-// the concept 
-int i; //int i = 0;
-for (i = 0; str[i] != '\0'; i++) //while(s[i] != '\0')
+int i;
+for (i = 0; str[i] != '\0'; i++) 
 {
-// In this second curly brace opening we would be
-// converting lowercase to uppercase, how?
-// the ASCII code for a-z are 97 - 122
-// the ASCII code for A-Z are 65 - 90
-// if you notice you will see the difference is 32
-// therefore to convert uppercase to lowercase
-// you will add 32
-// and to convert from lowercase to uppercase 
-// you will substract 32
 if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32; // can be rewritten this way
-// str[i] = str[i] - 32;
+str[i] -= 32;
 }
 return (str);
 }
