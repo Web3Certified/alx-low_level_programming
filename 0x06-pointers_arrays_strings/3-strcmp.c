5b0e84b5fd4038ec6862390d35f1cf157f584751
Web3Certified@gmail.com <crownprestige1@gmail.com>
2022-09-21 17:19:32 -0700

    cmp commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * @s2: string 2
  * Return: 0 if matching, and ns1 - ns2 if not matching
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 int i;
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
